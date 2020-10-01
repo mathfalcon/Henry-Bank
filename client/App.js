430adf3abd0e3ede58765a476005cc304fce0911
tomp <you@example.com>
2020-10-01 14:38:12 -0300

    Fase beta Alta de usuario

--- client/App.js
+++ client/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SignIn from './components/SignIn'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SignIn/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +14,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30
   },
 });
