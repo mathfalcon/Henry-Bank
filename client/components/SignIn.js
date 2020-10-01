f1c69699442cd6474e48315481bc398bc5f38441
tomp <you@example.com>
2020-10-01 17:21:41 -0300

    Registrate V2

--- client/components/SignIn.js
+++ client/components/SignIn.js
@@ -8,50 +8,65 @@ import {
   TextInput,
   Button,
   Alert,
+  ScrollView
 } from "react-native";
 
 function SignIn() {
   return (
-    <View style={style.container}>
+    <ScrollView>
+      <View style={style.container}>
       <View>
+        <Image
+          style={style.imagen}
+          source={require("../image/headerRegistro.png")}
+        />
         <Text style={style.titulo}>Alta de Cliente</Text>
       </View>
 
-      <View>
-
-      <RNPickerSelect
+      <View style={style.containerForm}>
+        <RNPickerSelect
           style={style.select}
           onValueChange={(value) => console.log(value)}
           placeholder={{
             label: "Elige un tipo de documento",
           }}
-          items={[{ label: "DNI", value: "DNI" }, { label: "Pasaporte", value: "Pasaporte" }, ]}
+          items={[
+            { label: "DNI", value: "DNI" },
+            { label: "Pasaporte", value: "Pasaporte" },
+          ]}
         />
-
-
 
         <TextInput placeholder="Documento" style={style.form}></TextInput>
         <TextInput placeholder="Nombre" style={style.form}></TextInput>
         <TextInput placeholder="Apellido" style={style.form}></TextInput>
-        <TextInput
-          placeholder="Fecha de nacimiento"
-          style={style.form}
-        ></TextInput>
+        <TextInput placeholder="Fecha de nacimiento" style={style.form}></TextInput>
+        <TextInput placeholder="Teléfono celular" style={style.form}></TextInput>
+        <TextInput placeholder="Domicilio Calle" style={style.form}></TextInput>
+        <TextInput placeholder="Número" style={style.form}></TextInput>
+        <TextInput placeholder="Localidad" style={style.form}></TextInput>
+        <TextInput placeholder="Provincia" style={style.form}></TextInput>
+        <TextInput placeholder="Pais" style={style.form}></TextInput>
+        
         <Button
           title="Registrate"
           onPress={() => Alert.alert("Probando mi botón")}
         />
       </View>
+
     </View>
+    </ScrollView>
   );
 }
 
 const style = StyleSheet.create({
   container: {
-    height: 900,
+    height: 1100,
     backgroundColor: "#eef0f2",
     flexDirection: "column",
     alignItems: "center",
+  },
+  containerForm: {
+    bottom: 70
   },
   form: {
     height: 45,
@@ -60,16 +75,22 @@ const style = StyleSheet.create({
     backgroundColor: "white",
     marginBottom: 20,
     paddingLeft: 20,
-    fontSize: 20
+    fontSize: 20,
   },
   titulo: {
-    color: "black", //de momento negro. Hace falta un fondo tipo henry
-    fontSize: 40,
-    margin: 30,
+    color: "white", //de momento negro. Hace falta un fondo tipo henry
+    fontSize: 35,
+    marginLeft: 80,
+    marginTop: 30,
+    bottom: 220
   },
   select: {
     width: 100,
-    backgroundColor:"red"
+    backgroundColor: "red",
+  },
+  imagen: {
+    width: 411,
+    height: 250,
   },
 });
 
