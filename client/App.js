94c1aa1bae28a333453b28b4ca202fd188308cd4
Marina Mastropierro <marinamastropierro8@gmail.com>
2020-10-02 20:04:03 -0300

    routes and navigation

--- client/App.js
+++ client/App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
-import LogIn from './components/LogIn'
+import Home from './components/Home';
+import LogIn from './components/LogIn';
+import SignIn from "./components/SignIn"
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LogIn/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="home" component={Home} />   
+        <Stack.Screen name="log" component={LogIn} />  
+        <Stack.Screen name="sign" component={SignIn} />  
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
