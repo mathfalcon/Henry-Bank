2c66287182dae6a992a2cf544f89b1ef05549a15
Marina Mastropierro <marinamastropierro8@gmail.com>
2020-10-02 14:16:01 -0300

    component login

--- client/App.js
+++ client/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SignIn from './components/SignIn'
+import LogIn from './components/LogIn'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <SignIn/>
+      <LogIn/>
       <StatusBar style="auto" />
     </View>
   );
