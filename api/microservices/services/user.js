fffa4cf4f71ad7045c7fb6f5ffb0bc23d0c5fd1f
Victor Guiliana <victorguiliana@gmail.com>
2020-10-03 00:20:27 -0300

    Route post user & comments

--- api/microservices/services/user.js
+++ api/microservices/services/user.js
@@ -27,40 +27,27 @@ server.get("/users", (req, res, next) => {
     .catch((err) => res.send(err));
 });
 
-// Route for posting a new user to db
+// Route for creating a new user to db
 server.post("/users", (req, res, next) => {
-  const { email, password, passcode, docType, docNumber, name, surname, birth, phone, street, street_number, locality, state, country, role } = req.body;
-
+  const { email, password, passcode, docType, docNumber, name, surname, birth, phone, street, street_number,
+         locality, state, country } = req.body;
   User.create({
-    email,
-    role,
-    password,
-    passcode,
-    docType,
-    docNumber,
-    name,
-    surname,
-    birth,
-    phone,
-    street,
-    street_number,
-    locality,
-    state,
-    country
+    email, role, password, passcode, docType, docNumber, name, surname, birth, phone, street, street_number,
+    locality, state, country
   }).then(userCreated => {
     res.status(200).send(userCreated)
   }).catch(err => res.send(err))
 });
-
+// Route to update an user information in db
 server.put("/users/update/:id", (req, res, next) => {
-  const { email, password, passcode, img, docType, docNumber, name, surname, birth, phone, street, street_number, locality, state, country, } = req.body;
+  const { email, password, passcode, img, docType, docNumber, name, surname, birth, phone, street, street_number, locality, state, country } = req.body;
   User.findByPk(req.params.id)
     .then(user => {
-      user.update({ email, password, passcode, img, docType, docNumber, name, surname, birth, phone, street, street_number, locality, state, country, })
+      user.update({ email, password, passcode, img, docType, docNumber, name, surname, birth, phone, street, street_number, locality, state, country })
     }).then((upduser) => res.send(upduser)
     ).catch((err) => res.status(400).send(err));
 });
-
+// Route to promote the user role to admin in db
 server.patch("/users/promote/:id", (req, res, next) => {
   User.findByPk(req.params.id)
     .then(user => {
@@ -68,7 +55,7 @@ server.patch("/users/promote/:id", (req, res, next) => {
     }).then((upduser) => res.send(upduser)
     ).catch((err) => res.status(400).send(err));
 });
-
+// Route to delete an user from db
 server.delete("/users/:id", (req, res) => {
   User.destroy({ where: { id: req.params.id } }).then((deletedRecord) => {
     if (deletedRecord === 1)
