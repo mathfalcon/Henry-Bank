a2dd72050aab79393abea47472442dbff71e3c83
unknown <drizzzt@gmail.com>
2020-10-05 14:26:30 -0300

    incomes & outcomes

--- api/microservices/services/user.js
+++ api/microservices/services/user.js
@@ -21,32 +21,43 @@ server.use(morgan("dev")); // Intializing console logger middleware for HTTP req
 // Route for getting all users
 server.get("/users", (req, res, next) => {
   User.findAll()
-    .then((users) => { res.status(200).send(users) })
-    .catch((err) => res.send(err));
+    .then((users) => { res.status(200).send({ success: true, message: "users: ", users }) })
+    .catch((err) => res.status(400).send({ success: false, message: "error: ", err }));
 });
 // Route for getting user income
 server.get("/users/income/:id", (req, res, next) => {
   Account.findOne({ where: { userId: id } })
     .then((account) => {
-      res.status(200).send(users)
+      Transaction.findAll({ where: { receiver: account.id } })
+        .then((transactions) => {
+          for (let i = 0, sum = 0; i < transactions.length; i++)
+            if (transactions[i].state === 'complete') sum += transactions[i].amount
+          res.send({ success: true, message: "Your incomes are: ", sum })
+        })
+        .catch((err) => res.status(400).send({ success: false, message: "error: ", err }));
     })
-    .catch((err) => res.send(err));
+    .catch((err) => res.status(400).send({ success: false, message: "error: ", err }));
 });
 // Route for getting user outcome
 server.get("/users/outcome/:id", (req, res, next) => {
   Account.findOne({ where: { userId: id } })
     .then((account) => {
-      Transaction.findAll({ where: { sender: account.id } });
-      res.status(200).send(users)
+      Transaction.findAll({ where: { sender: account.id } })
+        .then((transactions) => {
+          for (let i = 0, sum = 0; i < transactions.length; i++)
+            if (transactions[i].state === 'complete') sum += transactions[i].amount
+          res.send({ success: true, message: "Your outcomes are: ", sum })
+        })
+        .catch((err) => res.status(400).send({ success: false, message: "error: ", err }));
     })
-    .catch((err) => res.send(err));
+    .catch((err) => res.status(400).send({ success: false, message: "error: ", err }));
 });
 // Route for posting a new user 
 server.post("/users/create", (req, res, next) => {
   const { email, password, passcode, docType, docNumber, name, surname, birth, phone, street, street_number, locality, state, country, role } = req.body;
   User.create({ email, password, passcode, docType, docNumber, name, surname, birth, phone, street, street_number, locality, state, country, role })
     .then(userCreated => { res.status(200).send(userCreated) })
-    .catch(err => res.send(err))
+    .catch((err) => res.status(400).send({ success: false, message: "error: ", err }));
 });
 // Route to update an user information
 server.put("/users/update/:id", (req, res, next) => {
@@ -54,22 +65,24 @@ server.put("/users/update/:id", (req, res, next) => {
   User.findByPk(req.params.id)
     .then(user => { user.update({ email, password, passcode, docType, docNumber, name, surname, birth, phone, street, street_number, locality, state, country, role }) })
     .then((updatedUser) => res.send(updatedUser))
-    .catch((err) => res.status(400).send(err));
+    .catch((err) => res.status(400).send({ success: false, message: "error: ", err }));
 });
 // Route to promote the user role to admin 
 server.patch("/users/promote/:id", (req, res, next) => {
   User.findByPk(req.params.id)
     .then(user => { user.update({ role: "admin" }) })
     .then((updatedUser) => res.send(updatedUser))
-    .catch((err) => res.status(400).send(err));
+    .catch((err) => res.status(400).send({ success: false, message: "error: ", err }));
 });
 // Route to delete an user 
 server.delete("/users/:id", (req, res) => {
-  User.destroy({ where: { id: req.params.id } }).then((deletedRecord) => {
-    if (deletedRecord === 1)
-      res.status(200).json({ message: "User deleted" });
-    else res.status(400).json({ message: "User not found" });
-  });
+  User.destroy({ where: { id: req.params.id } })
+    .then((deletedRecord) => {
+      if (deletedRecord === 1)
+        res.status(200).json({ success: true, message: "User deleted" });
+      else res.status(400).json({ success: false, message: "User not found" });
+    })
+    .catch((err) => res.status(400).send({ success: false, message: "error: ", err }));
 });
 
 server.listen(3000, () => {
