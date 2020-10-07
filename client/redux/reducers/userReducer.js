433a31ccdb5494aef6dd58c32fa37d4fe923a369
Matias Bonino <matias2301@hotmail.com>
2020-10-06 23:09:38 -0300

    Connection to API location added and age verification in signupForm

--- client/redux/reducers/userReducer.js
+++ client/redux/reducers/userReducer.js
@@ -3,17 +3,21 @@ import * as C from "../constants";
 const initialState = {
   users: [],
   userCreationSuccess: false,
+  responseLocation: ""
 };
 
-function userReducer(state = initialState, action) {
-  console.log(action)
+function userReducer(state = initialState, action) {  
   switch (action.type) {
     case C.userCreated: {
       return { ...state, userCreationSuccess: action.payload.success };
+    }  
+  
+    case C.validateAddress: {      
+      return { ...state, responseLocation: action.payload };
     }
+
     default:
       return state;
-  }
+    }
 }
-
 export default userReducer;
