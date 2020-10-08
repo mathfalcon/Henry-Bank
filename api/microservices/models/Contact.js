bec1e96d83156e909ba0cd17e847138a654dcf05
Jose <joc13th@gmail.com>
2020-10-08 18:29:23 -0300

    Backend contac model

--- api/microservices/models/Contact.js
+++ api/microservices/models/Contact.js
@@ -1,5 +1,24 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("contact");
+  sequelize.define(
+    "contact",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      surname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nickname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+    });
 };
