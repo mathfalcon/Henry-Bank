045fa021be8f2ae153e5f2aee1dd01db01bb12f6
Jose <joc13th@gmail.com>
2020-10-08 19:10:55 -0300

    Contacts microservices and model 2.1

--- api/microservices/models/Contact.js
+++ api/microservices/models/Contact.js
@@ -1,31 +1,29 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "contact",
-    {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      surname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      nickname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        unique: true,
-      },
-      birth: {
-        type: DataTypes.DATEONLY,
-        allowNull: true,
-      },
-      phone: {
-        type: DataTypes.STRING,
-      },
-    });
+  sequelize.define("contact", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    surname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nickname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    birth: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+    },
+    phone: {
+      type: DataTypes.STRING,
+    },
+  });
 };
