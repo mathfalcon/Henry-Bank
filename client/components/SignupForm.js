dcc7ecb0fd7ce4542241eeb48ad86986cd377005
Matias Bonino <matias2301@hotmail.com>
2020-10-11 17:52:13 -0300

    Input confirmation fixed and horizontal navigation added in signup form

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,22 +1,58 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, CardStyleInterpolators, } from "@react-navigation/stack";
 import FirstPageForm from "./FirstPageForm";
 import SecondPageForm from "./SecondPageForm";
 import ThirdPageForm from "./ThirdPageForm";
-import TakePhoto from "./TakePhoto";
-import Photo from "./Photo";
+// import TakePhoto from "./TakePhoto";
+// import Photo from "./Photo";
 
 const Stack = createStackNavigator();
 
 export default function SignupForm() {
   return (          
-        <Stack.Navigator headerMode="none">          
-          <Stack.Screen name="first" component={FirstPageForm} />
-          <Stack.Screen name="second" component={SecondPageForm} />
-          <Stack.Screen name="third" component={ThirdPageForm} />
-          <Stack.Screen name="takePhoto" component={TakePhoto} />
-          <Stack.Screen name="photo" component={Photo} />
+        <Stack.Navigator
+          headerMode="none"
+          screenOptions={{               
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          }}
+        >               
+          <Stack.Screen
+            name="first"
+            component={FirstPageForm}
+          />
+
+        <Stack.Screen
+          name="second"
+          component={SecondPageForm}
+        />
+        
+        <Stack.Screen
+          name="third" 
+          component={ThirdPageForm}
+        />
+        
+        {/* <Stack.Screen
+          name="takePhoto"
+          component={TakePhoto}
+          options={{
+            transitionSpec: {
+            open: config,
+            close: config,
+            },
+          }}
+        />
+        
+        <Stack.Screen
+          name="photo"
+          component={Photo}
+          options={{
+            transitionSpec: {
+            open: config,
+            close: config,
+            },
+          }}
+        /> */}
+                
         </Stack.Navigator>      
   );
 }
