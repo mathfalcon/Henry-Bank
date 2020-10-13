38dafbc03715bbaa4414b3bc3ab6b9edc7adaa3e
tomp <you@example.com>
2020-10-13 14:16:55 -0300

    Arreglo el app

--- client/App.js
+++ client/App.js
@@ -5,9 +5,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import { getUserLogged } from "./redux/actions/authActions";
 import store from "./redux/store";
-// import Index from "./components";
+import Index from "./components";
 import * as Font from 'expo-font';
-import CreditCard from './components/MyCards';
 
 const Stack = createStackNavigator();
 
@@ -22,7 +21,7 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <CreditCard />
+      <Index />
     </Provider>
   );
 }
