3f283a3775d05853634c3bf285e8b3a548fe00d4
Agustin Dominguez <gusdomdominguez@gmail.com>
2020-10-20 20:21:06 -0300

    Created ForgotPassowrd and ResetPassword Components

--- client/components/index.js
+++ client/components/index.js
@@ -11,11 +11,12 @@ import Contacts from "./Contacts";
 import ForgotPass from "./ForgotPass";
 import Home from "./Home";
 import LogIn from "./LogIn";
-import MyCards from './MyCards';
+import MyCards from "./MyCards";
 import Position from "./Position";
 import RechargeMoney from "../views/RechargeMoney";
-import SendMoney from './SendMoney';
+import SendMoney from "./SendMoney";
 import SignupForm from "./SignupForm";
+import ResetPassword from "./ResetPassword";
 
 /* ------------------------------- ADMIN ------------------------------- */
 import AdminPanel from "./Admin/AdminPanel";
@@ -31,59 +32,71 @@ const AdminStack = createStackNavigator();
 export default function Index() {
   const dispatch = useDispatch();
   const userLogged = useSelector((state) => state.auth);
-  console.log('userLoggedRole',userLogged.user.role);
+  console.log("userLoggedRole", userLogged.user.role);
 
   useEffect(() => dispatch(getUserLogged()), []);
   return (
     <NavigationContainer>
-      
-        {
-          // La condicion "role === undefined" podria estar separada cargando un "spinner" si es "true"
-          userLogged.user.role === undefined || userLogged.user.role === 'guest' ? (
-            <>
+      {
+        // La condicion "role === undefined" podria estar separada cargando un "spinner" si es "true"
+        userLogged.user.role === undefined ||
+        userLogged.user.role === "guest" ? (
+          <>
             <LoggedFalseStack.Navigator headerMode="none">
               <LoggedFalseStack.Screen name="home" component={Home} />
               <LoggedFalseStack.Screen name="login" component={LogIn} />
               <LoggedFalseStack.Screen name="sign" component={SignupForm} />
-              <LoggedFalseStack.Screen name="forgotPass" component={ForgotPass} />
+              <LoggedFalseStack.Screen
+                name="forgotPass"
+                component={ForgotPass}
+              />
+              <LoggedFalseStack.Screen
+                name="resetPassword"
+                component={ResetPassword}
+              />
             </LoggedFalseStack.Navigator>
-            </>
-            )
-          :
-            userLogged.user.role === 'client'  ? (
-              <>
-              <LoggedTrueStack.Navigator headerMode="none">
-                <LoggedTrueStack.Screen name="position" component={Position} />
-                <LoggedTrueStack.Screen name="contacts" component={Contacts} />        
-                <LoggedTrueStack.Screen name="myCards" component={MyCards} />        
-                <LoggedTrueStack.Screen name="recharge" component={RechargeMoney} />
-                <LoggedTrueStack.Screen name="sendMoney" component={SendMoney} />
-                <LoggedTrueStack.Screen name="accountHistory" component={AccountHistory} />
-              </LoggedTrueStack.Navigator>
-              </>
-              )
-            :
-              (
-              <>
-              <AdminStack.Navigator headerMode="none">
-                <AdminStack.Screen name="adminPanel" component={AdminPanel} />
-                <AdminStack.Screen name="manageUsers" component={ManageUsers} />
-                <AdminStack.Screen name="manageAccounts" component={ManageAccounts} />
-                <AdminStack.Screen name="manageTransactions" component={ManageTransactions} />
-                <AdminStack.Screen name="seeStats" component={SeeStats} />
-              </AdminStack.Navigator>                
-              </>
-              )
-        }
-      
+          </>
+        ) : userLogged.user.role === "client" ? (
+          <>
+            <LoggedTrueStack.Navigator headerMode="none">
+              <LoggedTrueStack.Screen name="position" component={Position} />
+              <LoggedTrueStack.Screen name="contacts" component={Contacts} />
+              <LoggedTrueStack.Screen name="myCards" component={MyCards} />
+              <LoggedTrueStack.Screen
+                name="recharge"
+                component={RechargeMoney}
+              />
+              <LoggedTrueStack.Screen name="sendMoney" component={SendMoney} />
+              <LoggedTrueStack.Screen
+                name="accountHistory"
+                component={AccountHistory}
+              />
+            </LoggedTrueStack.Navigator>
+          </>
+        ) : (
+          <>
+            <AdminStack.Navigator headerMode="none">
+              <AdminStack.Screen name="adminPanel" component={AdminPanel} />
+              <AdminStack.Screen name="manageUsers" component={ManageUsers} />
+              <AdminStack.Screen
+                name="manageAccounts"
+                component={ManageAccounts}
+              />
+              <AdminStack.Screen
+                name="manageTransactions"
+                component={ManageTransactions}
+              />
+              <AdminStack.Screen name="seeStats" component={SeeStats} />
+            </AdminStack.Navigator>
+          </>
+        )
+      }
     </NavigationContainer>
   );
 }
 
-              // De volver a la estructura que estaba ( la que quedó comentada abajo, descomentar linea 52 del componente login y borrar la 53
-              // y descomentar la linea 36 del componente position y borrar la 37 )
-
-
+// De volver a la estructura que estaba ( la que quedó comentada abajo, descomentar linea 52 del componente login y borrar la 53
+// y descomentar la linea 36 del componente position y borrar la 37 )
 
 // import "react-native-gesture-handler";
 // import React, { useEffect } from "react";
@@ -115,30 +128,30 @@ export default function Index() {
 
 // export default function Index() {
 //   const dispatch = useDispatch();
-//   const userLogged = useSelector((state) => state.auth);  
+//   const userLogged = useSelector((state) => state.auth);
 
 //   useEffect(() => dispatch(getUserLogged()), []);
 //   return (
-//     <NavigationContainer>        
+//     <NavigationContainer>
 //             <Stack.Navigator headerMode="none">
 //               <Stack.Screen name="home" component={Home} />
 //               <Stack.Screen name="login" component={LogIn} />
 //               <Stack.Screen name="sign" component={SignupForm} />
 //               <Stack.Screen name="forgotPass" component={ForgotPass} />
-         
+
 //                 <Stack.Screen name="position" component={Position} />
-//                 <Stack.Screen name="contacts" component={Contacts} />        
-//                 <Stack.Screen name="myCards" component={MyCards} />        
+//                 <Stack.Screen name="contacts" component={Contacts} />
+//                 <Stack.Screen name="myCards" component={MyCards} />
 //                 <Stack.Screen name="recharge" component={RechargeMoney} />
 //                 <Stack.Screen name="sendMoney" component={SendMoney} />
 //                 <Stack.Screen name="accountHistory" component={AccountHistory} />
-           
+
 //                 <Stack.Screen name="adminPanel" component={AdminPanel} />
 //                 <Stack.Screen name="manageUsers" component={ManageUsers} />
 //                 <Stack.Screen name="manageAccounts" component={ManageAccounts} />
 //                 <Stack.Screen name="manageTransactions" component={ManageTransactions} />
 //                 <Stack.Screen name="seeStats" component={SeeStats} />
-//         </Stack.Navigator>                
+//         </Stack.Navigator>
 //     </NavigationContainer>
 //   );
 // }
