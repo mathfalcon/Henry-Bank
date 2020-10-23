82dbe847bc18e64e7aa9f5a27a421ada23a746c2
Matias Bonino <matias2301@hotmail.com>
2020-10-23 19:08:03 -0300

    Keyboard for getting into MyCards component created

--- client/components/MenuOperation.js
+++ client/components/MenuOperation.js
@@ -1,68 +1,168 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import VirtualKeyboard from 'react-native-virtual-keyboard';
+import { Modal, View, TouchableHighlight } from "react-native";
+import axios from "axios";
+import { api } from "./Constants/constants";
+import styles from "../Styles/keyboardStyles";
 import {
   Container,
   Header,
   Content,
-  Footer,
+  Footer, 
+  Input,
   FooterTab,
   Button,
   Icon,
   Text,
 } from "native-base";
 
-export default MenuOperation = ({ navigation, screen }) => {
-  console.log("route.params", screen);
+export default MenuOperation = ({ navigation, screen, userLogged }) => {
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [inputKeyboard, setInputKeyboard] = useState("");  
+  
+  useEffect(() => {
+    if ( inputKeyboard.length === 4 ){      
+      let values = {
+        userId: userLogged.user.id,
+        passcode: inputKeyboard,
+      }     
+      
+      axios
+      .post(`${api}/auth/check-passcode`,values)
+        .then((response) => {          
+          if(response.data.success){             
+            setInputKeyboard("");
+            setModalVisible(false);                   
+            navigation.navigate("myCards")            
+          }       
+        })
+        .catch(error => console.log("Something went wrong"));
+      } 
+  }, [inputKeyboard]);
 
   return (
-    <FooterTab style={{ backgroundColor: "#151515" }}>
-      <Button vertical onPress={() => navigation.navigate("userStats")}>
-        <Icon
-          name="chart-bar"
-          type="FontAwesome5"
-          style={screen !== "stats" ? { color: "white" } : { color: "#ffff6d" }}
-        />
-        <Text style={{ color: "white" }}>STATS</Text>
-      </Button>
+    <>
+      <FooterTab style={{ backgroundColor: "#151515" }}>
+        <Button vertical onPress={() => navigation.navigate("userStats")}>
+          <Icon
+            name="chart-bar"
+            type="FontAwesome5"
+            style={screen !== "stats" ? { color: "white" } : { color: "#ffff6d" }}
+          />
+          <Text style={{ color: "white" }}>STATS</Text>
+        </Button>
 
-      <Button vertical onPress={() => navigation.navigate("myCards")}>
-        <Icon
-          name="card"
-          style={screen !== "cards" ? { color: "white" } : { color: "#ffff6d" }}
-        />
-        <Text style={{ color: "white" }}>Cards</Text>
-      </Button>
-      <Button vertical onPress={() => navigation.navigate("position")}>
-        <Icon
-          name="home"
-          type="FontAwesome5"
-          style={
-            screen !== "position" ? { color: "white" } : { color: "#ffff6d" }
-          }
-        />
-        <Text style={{ color: "white" }}>HOME</Text>
-      </Button>
+        <Button vertical onPress={() => setModalVisible(!modalVisible)}>
+          <Icon
+            name="card"
+            style={screen !== "cards" ? { color: "white" } : { color: "#ffff6d" }}
+          />
+          <Text style={{ color: "white" }}>Cards</Text>
+        </Button>
+        <Button vertical onPress={() => navigation.navigate("position")}>
+          <Icon
+            name="home"
+            type="FontAwesome5"
+            style={
+              screen !== "position" ? { color: "white" } : { color: "#ffff6d" }
+            }
+          />
+          <Text style={{ color: "white" }}>HOME</Text>
+        </Button>
 
-      <Button vertical onPress={() => navigation.navigate("accountHistory")}>
-        <Icon
-          style={
-            screen !== "accounts" ? { color: "white" } : { color: "#ffff6d" }
-          }
-          type="FontAwesome"
-          name="dollar"
-        />
-        <Text style={{ color: "white" }}>History</Text>
-      </Button>
+        <Button vertical onPress={() => navigation.navigate("accountHistory")}>
+          <Icon
+            style={
+              screen !== "accounts" ? { color: "white" } : { color: "#ffff6d" }
+            }
+            type="FontAwesome"
+            name="dollar"
+          />
+          <Text style={{ color: "white" }}>History</Text>
+        </Button>
 
-      <Button vertical onPress={() => navigation.navigate("contacts")}>
-        <Icon
-          name="users"
-          type="FontAwesome5"
-          style={
-            screen !== "contacts" ? { color: "white" } : { color: "#ffff6d" }
-          }
-        />
-        <Text style={{ color: "white" }}>Friend</Text>
-      </Button>
-    </FooterTab>
+        <Button vertical onPress={() => navigation.navigate("contacts")}>
+          <Icon
+            name="users"
+            type="FontAwesome5"
+            style={
+              screen !== "contacts" ? { color: "white" } : { color: "#ffff6d" }
+            }
+          />
+          <Text style={{ color: "white" }}>Friend</Text>
+        </Button>
+      </FooterTab>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Close modal first");
+        }}
+      >                        
+        <View style={styles.modalView}>
+
+        <Text style={styles.modalText}>Enter your PassCode</Text>  
+          
+          <View style={styles.inputs}>
+            <Input
+              secureTextEntry={true}    
+              disabled={true}
+              rounded
+              name="inputKeyboard[0]"
+              value={inputKeyboard[0]}              
+              style={styles.textInput}     
+            />
+            <Input
+              secureTextEntry={true}   
+              disabled={true}
+              rounded
+              name="inputKeyboard[1]"
+              value={inputKeyboard[1]}               
+              style={styles.textInput}     
+            />
+            <Input
+              secureTextEntry={true}  
+              disabled={true}
+              rounded
+              name="inputKeyboard[2]"
+              value={inputKeyboard[2]}                
+              style={styles.textInput}     
+            />
+            <Input
+              secureTextEntry={true}  
+              disabled={true}
+              rounded
+              name="inputKeyboard[3]"
+              value={inputKeyboard[3]}               
+              style={styles.textInput}     
+            />
+          </View>
+
+          <VirtualKeyboard
+            color='white'
+            pressMode='string'
+            onPress={(val) => setInputKeyboard(val)}  
+            style={styles.keyboard}         
+          />
+
+          <TouchableHighlight
+            style={styles.backButtom}
+            onPress={() => {
+              setInputKeyboard("");
+              setModalVisible(!modalVisible)
+              }
+            }
+          >
+            <Text style={styles.textStyle}>
+              BACK
+            </Text>
+          </TouchableHighlight>
+
+        </View>
+      </Modal>                        
+    </>
   );
 };
