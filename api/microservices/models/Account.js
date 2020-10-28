950760bf3ce7fe03ef3b7323698b8854483aad64
unknown <drizzzt@gmail.com>
2020-10-28 11:03:13 -0300

    added cvu to account model.

--- api/microservices/models/Account.js
+++ api/microservices/models/Account.js
@@ -3,14 +3,18 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define("account", {
     balance: {
-        type: DataTypes.DECIMAL,
-        defaultValue: 0.0,
-        validate: { min: 0.0 },
+      type: DataTypes.DECIMAL,
+      defaultValue: 0.0,
+      validate: { min: 0.0 },
     },
     type: {
-        type: DataTypes.ENUM,
-        defaultValue: 'AR$',
-        values: ['USD', 'AR$']
+      type: DataTypes.ENUM,
+      defaultValue: 'AR$',
+      values: ['USD', 'AR$']
+    },
+    cvu: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
   });
 };
